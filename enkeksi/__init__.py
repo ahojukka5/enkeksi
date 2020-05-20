c0995d86d99ea12b502cae13a27dc025a56050a9
Jukka Aho <ahojukka5@gmail.com>
2020-05-20 21:28:51 +0700

    From stuff from public api definition

--- enkeksi/__init__.py
+++ enkeksi/__init__.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .mdprocess import get_cursor, process
-
 __version__ = "0.1.3"
 __author__ = "Jukka Aho <ahojukka5@gmail.com>"
-__all__ = [get_cursor, process]
