7e5da874540614918e9b2d1838b2ca6e7cee2581
Jukka Aho <ahojukka5@gmail.com>
2020-05-20 15:56:46 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,26 +37,27 @@ extras = {
 }
 
 description = """
-markdown-sql-eval takes a markdown-formatted input and executes the sql queries
-found in it, and returns a markdown-formatted output where the results of the
-sql queries have been added. markdown-sql-eval comes with a command line tool
-with a same name which can be used to process markdown files. Project is
-developed and hosted in GitHub: https://github.com/ahojukka5/gmshparser.
+enkeksi takes a markdown-formatted input and executes the sql queries found in
+it, and returns a pretty markdown-formatted output where the results of the SQL
+queries have been added. To make usage of package easy, enkeksi comes with a
+command line tool markdown-sql-eval which can be used to process markdown
+files. Project is developed and hosted in GitHub:
+https://github.com/ahojukka5/enkeksi.
 """
 
 entry_points = {
-    'console_scripts': ['markdown-sql-eval=markdown_sql_eval.cli:main'],
+    'console_scripts': ['markdown-sql-eval=enkeksi.cli:main'],
 }
 
 setuptools.setup(
-    name="markdown_sql_eval",
-    version=get_version("markdown_sql_eval/__init__.py"),
+    name="enkeksi",
+    version=get_version("enkeksi/__init__.py"),
     author="Jukka Aho",
     author_email="ahojukka5@gmail.com",
     description=" ".join(description.strip().splitlines()),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ahojukka5/markdown-sql-eval",
+    url="https://github.com/ahojukka5/enkeksi",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
