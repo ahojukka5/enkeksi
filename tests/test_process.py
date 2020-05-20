1549995da8f0e4afaaa88546988c85b4f89a2d1d
Jukka Aho <ahojukka5@gmail.com>
2020-05-20 16:30:29 +0700

    Add more tests to process.py

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,15 +1,45 @@
 import io
 import textwrap
+import pytest
+
 from enkeksi import get_cursor, process
 
 
-def test_process():
-    output = io.StringIO()
+@pytest.fixture
+def cursor():
+    return get_cursor()
+
+
+@pytest.fixture
+def output():
+    return io.StringIO()
+
+
+def test_process(cursor, output):
     block = textwrap.dedent("""
     ```sql
     SELECT 1+1;
     ```
     """)
-    cursor = get_cursor()
     process(cursor, block, output, show_headers=False)
     assert "2" in output.getvalue()
+
+
+def test_process_normal_text(cursor, output):
+    block = textwrap.dedent("""
+    Hello, World!
+    """)
+    process(cursor, block, output, show_headers=False)
+    assert "Hello" in output.getvalue()
+
+
+def test_block_process(cursor, output):
+    block = textwrap.dedent("""
+    ```sql
+    CREATE TABLE Foo (id INTEGER PRIMARY KEY, data INTEGER);
+    INSERT INTO Foo (data) VALUES (1);
+    ```
+    """)
+    process(cursor, block, output, show_headers=False)
+    cursor.execute("SELECT * FROM Foo")
+    assert len(cursor.fetchall()) == 1
