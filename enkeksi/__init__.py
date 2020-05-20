956d6930b75a9f697ff666fabc84f6d2f0365f7e
Jukka Aho <ahojukka5@gmail.com>
2020-05-20 16:29:33 +0700

    Rename process.py to mdprocess.py

--- enkeksi/__init__.py
+++ enkeksi/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+from .mdprocess import get_cursor, process
 
 __version__ = "0.1.0"
 __author__ = "Jukka Aho <ahojukka5@gmail.com>"
-
-from .process import get_cursor, process
-
 __all__ = [get_cursor, process]
