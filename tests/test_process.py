81bc791b4dfb09862e691cec4be1af01df6f9d56
Jukka Aho <ahojukka5@gmail.com>
2020-05-20 21:29:41 +0700

    More tests
    - parse_header
    - check failure of executing sql

--- tests/test_process.py
+++ tests/test_process.py
@@ -2,7 +2,7 @@ import io
 import textwrap
 import pytest
 
-from enkeksi import get_cursor, process
+from enkeksi.mdprocess import get_cursor, process, parse_header
 
 
 @pytest.fixture
@@ -25,6 +25,29 @@ def test_process(cursor, output):
     assert "2" in output.getvalue()
 
 
+def test_process_caption(cursor, output):
+    block = textwrap.dedent("""
+    ```sql caption="hello"
+    SELECT 1+1;
+    ```
+    """)
+    process(cursor, block, output, show_headers=False)
+    assert "hello" in output.getvalue()
+
+
+def test_process_failure(cursor, output):
+    block = textwrap.dedent("""
+    ```sql
+    SELCT 1+1;
+    ```
+    """)
+    process(cursor, block, output, show_headers=False)
+    print("test_process_failure")
+    print(output.getvalue())
+    print("test_process_failure end")
+    assert "Error" in output.getvalue()
+
+
 def test_process_normal_text(cursor, output):
     block = textwrap.dedent("""
     Hello, World!
@@ -43,3 +66,12 @@ def test_block_process(cursor, output):
     process(cursor, block, output, show_headers=False)
     cursor.execute("SELECT * FROM Foo")
     assert len(cursor.fetchall()) == 1
+
+
+def test_parse_header():
+    s = '```sql hide_input caption="Hello World"'
+    p = parse_header(s)
+    assert 'sql' in p
+    assert 'hide_input' in p
+    assert 'caption' in p
+    assert p["caption"] == "Hello World"
